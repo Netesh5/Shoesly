d825634c5f7ea16cb00402f5fa985fd2441226b8
Netesh5 <netesh.paudel@gmail.com>
2024-06-07 21:50:47 +0545

    feat: added appcolor and textstyle

---
+++ lib/core/themes/app_colors.dart
@@ -0,0 +1,273 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  /// brandGreen900 figma properties
+  /// hex: #62740d
+  /// opacity: 1
+  static const Color brandGreen900 = Color(0xff62740d);
+
+  /// brandGreen800 figma properties
+  /// hex: #798c16
+  /// opacity: 1
+  static const Color brandGreen800 = Color(0xff798c16);
+
+  /// brandGreen700 figma properties
+  /// hex: #99ae23
+  /// opacity: 1
+  static const Color brandGreen700 = Color(0xff99ae23);
+
+  /// brandGreen600 figma properties
+  /// hex: #bad033
+  /// opacity: 1
+  static const Color brandGreen600 = Color(0xffbad033);
+
+  /// brandGreen500 figma properties
+  /// hex: #ddf247
+  /// opacity: 1
+  static const Color brandGreen500 = Color(0xffddf247);
+
+  /// brandGreen400 figma properties
+  /// hex: #e8f774
+  /// opacity: 1
+  static const Color brandGreen400 = Color(0xffe8f774);
+
+  /// brandGreen300 figma properties
+  /// hex: #f0fb90
+  /// opacity: 1
+  static const Color brandGreen300 = Color(0xfff0fb90);
+
+  /// brandGreen200 figma properties
+  /// hex: #f7fdb5
+  /// opacity: 1
+  static const Color brandGreen200 = Color(0xfff7fdb5);
+
+  /// brandGreen100 figma properties
+  /// hex: #fbfeda
+  /// opacity: 1
+  static const Color brandGreen100 = Color(0xfffbfeda);
+
+  /// primarySucess900 figma properties
+  /// hex: #0f595a
+  /// opacity: 1
+  static const Color primarySucess900 = Color(0xff0f595a);
+
+  /// primarySucess800 figma properties
+  /// hex: #1a6d66
+  /// opacity: 1
+  static const Color primarySucess800 = Color(0xff1a6d66);
+
+  /// primarySucess700 figma properties
+  /// hex: #298876
+  /// opacity: 1
+  static const Color primarySucess700 = Color(0xff298876);
+
+  /// primarySucess600 figma properties
+  /// hex: #3ba285
+  /// opacity: 1
+  static const Color primarySucess600 = Color(0xff3ba285);
+
+  /// primarySucess500 figma properties
+  /// hex: #52bd94
+  /// opacity: 1
+  static const Color primarySucess500 = Color(0xff52bd94);
+
+  /// primarySucess400 figma properties
+  /// hex: #7bd7ab
+  /// opacity: 1
+  static const Color primarySucess400 = Color(0xff7bd7ab);
+
+  /// primarySucess300 figma properties
+  /// hex: #9bebbc
+  /// opacity: 1
+  static const Color primarySucess300 = Color(0xff9bebbc);
+
+  /// primarySucess200 figma properties
+  /// hex: #a1f9b4
+  /// opacity: 1
+  static const Color primarySucess200 = Color(0xffa1f9b4);
+
+  /// primarySucess100 figma properties
+  /// hex: #edf8ef
+  /// opacity: 1
+  static const Color primarySucess100 = Color(0xffedf8ef);
+
+  /// primaryError900 figma properties
+  /// hex: #7a0e40
+  /// opacity: 1
+  static const Color primaryError900 = Color(0xff7a0e40);
+
+  /// primaryError800 figma properties
+  /// hex: #931846
+  /// opacity: 1
+  static const Color primaryError800 = Color(0xff931846);
+
+  /// primaryError700 figma properties
+  /// hex: #b7264f
+  /// opacity: 1
+  static const Color primaryError700 = Color(0xffb7264f);
+
+  /// primaryError600 figma properties
+  /// hex: #db3756
+  /// opacity: 1
+  static const Color primaryError600 = Color(0xffdb3756);
+
+  /// primaryError500 figma properties
+  /// hex: #ff4c5e
+  /// opacity: 1
+  static const Color primaryError500 = Color(0xffff4c5e);
+
+  /// primaryError400 figma properties
+  /// hex: #ff7879
+  /// opacity: 1
+  static const Color primaryError400 = Color(0xffff7879);
+
+  /// primaryError300 figma properties
+  /// hex: #ff9c93
+  /// opacity: 1
+  static const Color primaryError300 = Color(0xffff9c93);
+
+  /// primaryError200 figma properties
+  /// hex: #ffc3b7
+  /// opacity: 1
+  static const Color primaryError200 = Color(0xffffc3b7);
+
+  /// primaryError100 figma properties
+  /// hex: #fff5f1
+  /// opacity: 1
+  static const Color primaryError100 = Color(0xfffff5f1);
+
+  /// primaryWarning900 figma properties
+  /// hex: #78560c
+  /// opacity: 1
+  static const Color primaryWarning900 = Color(0xff78560c);
+
+  /// primaryWarning800 figma properties
+  /// hex: #926d14
+  /// opacity: 1
+  static const Color primaryWarning800 = Color(0xff926d14);
+
+  /// primaryWarning700 figma properties
+  /// hex: #b58d20
+  /// opacity: 1
+  static const Color primaryWarning700 = Color(0xffb58d20);
+
+  /// primaryWarning600 figma properties
+  /// hex: #d8ae2e
+  /// opacity: 1
+  static const Color primaryWarning600 = Color(0xffd8ae2e);
+
+  /// primaryWarning500 figma properties
+  /// hex: #fcd240
+  /// opacity: 1
+  static const Color primaryWarning500 = Color(0xfffcd240);
+
+  /// primaryWarning400 figma properties
+  /// hex: #fde06f
+  /// opacity: 1
+  static const Color primaryWarning400 = Color(0xfffde06f);
+
+  /// primaryWarning300 figma properties
+  /// hex: #fee98c
+  /// opacity: 1
+  static const Color primaryWarning300 = Color(0xfffee98c);
+
+  /// primaryWarning200 figma properties
+  /// hex: #fef3b2
+  /// opacity: 1
+  static const Color primaryWarning200 = Color(0xfffef3b2);
+
+  /// primaryWarning100 figma properties
+  /// hex: #fefbe7
+  /// opacity: 1
+  static const Color primaryWarning100 = Color(0xfffefbe7);
+
+  /// primaryInformation900 figma properties
+  /// hex: #071561
+  /// opacity: 1
+  static const Color primaryInformation900 = Color(0xff071561);
+
+  /// primaryInformation800 figma properties
+  /// hex: #0d1f76
+  /// opacity: 1
+  static const Color primaryInformation800 = Color(0xff0d1f76);
+
+  /// primaryInformation700 figma properties
+  /// hex: #142d92
+  /// opacity: 1
+  static const Color primaryInformation700 = Color(0xff142d92);
+
+  /// primaryInformation600 figma properties
+  /// hex: #1d3eaf
+  /// opacity: 1
+  static const Color primaryInformation600 = Color(0xff1d3eaf);
+
+  /// primaryInformation500 figma properties
+  /// hex: #2952cc
+  /// opacity: 1
+  static const Color primaryInformation500 = Color(0xff2952cc);
+
+  /// primaryInformation400 figma properties
+  /// hex: #597fe0
+  /// opacity: 1
+  static const Color primaryInformation400 = Color(0xff597fe0);
+
+  /// primaryInformation300 figma properties
+  /// hex: #7c9fef
+  /// opacity: 1
+  static const Color primaryInformation300 = Color(0xff7c9fef);
+
+  /// primaryInformation200 figma properties
+  /// hex: #aac4f9
+  /// opacity: 1
+  static const Color primaryInformation200 = Color(0xffaac4f9);
+
+  /// primaryInformation100 figma properties
+  /// hex: #eaf1ff
+  /// opacity: 1
+  static const Color primaryInformation100 = Color(0xffeaf1ff);
+
+  /// secondinary900 figma properties
+  /// hex: #74100d
+  /// opacity: 1
+  static const Color secondinary900 = Color(0xff74100d);
+
+  /// secondinary800 figma properties
+  /// hex: #8c2216
+  /// opacity: 1
+  static const Color secondinary800 = Color(0xff8c2216);
+
+  /// secondinary700 figma properties
+  /// hex: #ae3a23
+  /// opacity: 1
+  static const Color secondinary700 = Color(0xffae3a23);
+
+  /// secondinary600 figma properties
+  /// hex: #d05833
+  /// opacity: 1
+  static const Color secondinary600 = Color(0xffd05833);
+
+  /// secondinary500 figma properties
+  /// hex: #f27a47
+  /// opacity: 1
+  static const Color secondinary500 = Color(0xfff27a47);
+
+  /// secondinary400 figma properties
+  /// hex: #f7a474
+  /// opacity: 1
+  static const Color secondinary400 = Color(0xfff7a474);
+
+  /// secondinary300 figma properties
+  /// hex: #fbbe90
+  /// opacity: 1
+  static const Color secondinary300 = Color(0xfffbbe90);
+
+  /// secondinary200 figma properties
+  /// hex: #fdd9b5
+  /// opacity: 1
+  static const Color secondinary200 = Color(0xfffdd9b5);
+
+  /// secondinary100 figma properties
+  /// hex: #feeeda
+  /// opacity: 1
+  static const Color secondinary100 = Color(0xfffeeeda);
+}
---
+++ lib/core/themes/app_text.dart
@@ -0,0 +1,291 @@
+import 'package:flutter/widgets.dart';
+
+abstract class AppTextStyle {
+  /// headline900 figma properties
+  /// fontFamily: Urbanist
+  /// fontSize: 70px
+  /// height: 150%
+  /// fontWeight: 600
+  /// letterSpacing: 1%
+  /// fontStyle: none
+  /// decoration: none
+  static const TextStyle headline900 = TextStyle(
+    fontFamily: 'Urbanist',
+    fontSize: 70,
+    height: 1.5,
+    fontWeight: FontWeight.w600,
+    letterSpacing: 0.7,
+  );
+
+  /// headline900 figma properties
+  /// fontFamily: Urbanist
+  /// fontSize: 48px
+  /// height: 58px
+  /// fontWeight: 700
+  /// letterSpacing: 1%
+  /// fontStyle: none
+  /// decoration: none
+  static const TextStyle headline90048 = TextStyle(
+    fontFamily: 'Urbanist',
+    fontSize: 48,
+    height: 1.21,
+    fontWeight: FontWeight.w700,
+    letterSpacing: 0.48,
+  );
+
+  /// headline800 figma properties
+  /// fontFamily: Urbanist
+  /// fontSize: 36px
+  /// height: 46px
+  /// fontWeight: 600
+  /// letterSpacing: 1%
+  /// fontStyle: none
+  /// decoration: none
+  static const TextStyle headline800 = TextStyle(
+    fontFamily: 'Urbanist',
+    fontSize: 36,
+    height: 1.28,
+    fontWeight: FontWeight.w600,
+    letterSpacing: 0.36,
+  );
+
+  /// headline700 figma properties
+  /// fontFamily: Urbanist
+  /// fontSize: 30px
+  /// height: 45px
+  /// fontWeight: 700
+  /// letterSpacing: 0.3px
+  /// fontStyle: none
+  /// decoration: none
+  static const TextStyle headline700 = TextStyle(
+    fontFamily: 'Urbanist',
+    fontSize: 30,
+    height: 1.5,
+    fontWeight: FontWeight.w700,
+    letterSpacing: 0.3,
+  );
+
+  /// headline700 figma properties
+  /// fontFamily: Urbanist
+  /// fontSize: 24px
+  /// height: 34px
+  /// fontWeight: 600
+  /// letterSpacing: 1%
+  /// fontStyle: none
+  /// decoration: none
+  static const TextStyle headline70024 = TextStyle(
+    fontFamily: 'Urbanist',
+    fontSize: 24,
+    height: 1.42,
+    fontWeight: FontWeight.w600,
+    letterSpacing: 0.24,
+  );
+
+  /// headline600 figma properties
+  /// fontFamily: Urbanist
+  /// fontSize: 20px
+  /// height: 30px
+  /// fontWeight: 700
+  /// letterSpacing: 1%
+  /// fontStyle: none
+  /// decoration: none
+  static const TextStyle headline600 = TextStyle(
+    fontFamily: 'Urbanist',
+    fontSize: 20,
+    height: 1.5,
+    fontWeight: FontWeight.w700,
+    letterSpacing: 0.2,
+  );
+
+  /// headline500 figma properties
+  /// fontFamily: Urbanist
+  /// fontSize: 18px
+  /// height: 26px
+  /// fontWeight: 700
+  /// letterSpacing: 1%
+  /// fontStyle: none
+  /// decoration: none
+  static const TextStyle headline500 = TextStyle(
+    fontFamily: 'Urbanist',
+    fontSize: 18,
+    height: 1.44,
+    fontWeight: FontWeight.w700,
+    letterSpacing: 0.18,
+  );
+
+  /// headline400 figma properties
+  /// fontFamily: Urbanist
+  /// fontSize: 16px
+  /// height: 26px
+  /// fontWeight: 600
+  /// letterSpacing: 1%
+  /// fontStyle: none
+  /// decoration: none
+  static const TextStyle headline400 = TextStyle(
+    fontFamily: 'Urbanist',
+    fontSize: 16,
+    height: 1.63,
+    fontWeight: FontWeight.w600,
+    letterSpacing: 0.16,
+  );
+
+  /// headline300 figma properties
+  /// fontFamily: Urbanist
+  /// fontSize: 14px
+  /// height: 24px
+  /// fontWeight: 700
+  /// letterSpacing: 1%
+  /// fontStyle: none
+  /// decoration: none
+  static const TextStyle headline300 = TextStyle(
+    fontFamily: 'Urbanist',
+    fontSize: 14,
+    height: 1.71,
+    fontWeight: FontWeight.w700,
+    letterSpacing: 0.14,
+  );
+
+  /// headline200 figma properties
+  /// fontFamily: Urbanist
+  /// fontSize: 12px
+  /// height: 22px
+  /// fontWeight: 700
+  /// letterSpacing: 1%
+  /// fontStyle: none
+  /// decoration: none
+  static const TextStyle headline200 = TextStyle(
+    fontFamily: 'Urbanist',
+    fontSize: 12,
+    height: 1.83,
+    fontWeight: FontWeight.w700,
+    letterSpacing: 0.12,
+  );
+
+  /// headline100 figma properties
+  /// fontFamily: Urbanist
+  /// fontSize: 10px
+  /// height: 20px
+  /// fontWeight: 500
+  /// letterSpacing: 1%
+  /// fontStyle: none
+  /// decoration: none
+  static const TextStyle headline100 = TextStyle(
+    fontFamily: 'Urbanist',
+    fontSize: 10,
+    height: 2,
+    fontWeight: FontWeight.w500,
+    letterSpacing: 0.1,
+  );
+
+  /// heading300 figma properties
+  /// fontFamily: Urbanist
+  /// fontSize: 14px
+  /// height: 150%
+  /// fontWeight: 700
+  /// letterSpacing: 1%
+  /// fontStyle: none
+  /// decoration: none
+  static const TextStyle heading300 = TextStyle(
+    fontFamily: 'Urbanist',
+    fontSize: 14,
+    height: 1.5,
+    fontWeight: FontWeight.w700,
+    letterSpacing: 0.14,
+  );
+
+  /// heading200 figma properties
+  /// fontFamily: Urbanist
+  /// fontSize: 12px
+  /// height: 22px
+  /// fontWeight: 600
+  /// letterSpacing: 1%
+  /// fontStyle: none
+  /// decoration: none
+  static const TextStyle heading200 = TextStyle(
+    fontFamily: 'Urbanist',
+    fontSize: 12,
+    height: 1.83,
+    fontWeight: FontWeight.w600,
+    letterSpacing: 0.12,
+  );
+
+  /// heading100 figma properties
+  /// fontFamily: Urbanist
+  /// fontSize: 10px
+  /// height: 20px
+  /// fontWeight: 500
+  /// letterSpacing: 1%
+  /// fontStyle: none
+  /// decoration: none
+  static const TextStyle heading100 = TextStyle(
+    fontFamily: 'Urbanist',
+    fontSize: 10,
+    height: 2,
+    fontWeight: FontWeight.w500,
+    letterSpacing: 0.1,
+  );
+
+  /// bodytext300 figma properties
+  /// fontFamily: Urbanist
+  /// fontSize: 16px
+  /// height: 26px
+  /// fontWeight: 400
+  /// letterSpacing: 1%
+  /// fontStyle: none
+  /// decoration: none
+  static const TextStyle bodytext300 = TextStyle(
+    fontFamily: 'Urbanist',
+    fontSize: 16,
+    height: 1.63,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.16,
+  );
+
+  /// bodytext200 figma properties
+  /// fontFamily: Urbanist
+  /// fontSize: 14px
+  /// height: 24px
+  /// fontWeight: 400
+  /// letterSpacing: 1%
+  /// fontStyle: none
+  /// decoration: none
+  static const TextStyle bodytext200 = TextStyle(
+    fontFamily: 'Urbanist',
+    fontSize: 14,
+    height: 1.71,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.14,
+  );
+
+  /// bodytext100 figma properties
+  /// fontFamily: Urbanist
+  /// fontSize: 12px
+  /// height: 22px
+  /// fontWeight: 400
+  /// letterSpacing: 1%
+  /// fontStyle: none
+  /// decoration: none
+  static const TextStyle bodytext100 = TextStyle(
+    fontFamily: 'Urbanist',
+    fontSize: 12,
+    height: 1.83,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.12,
+  );
+
+  /// bodytext10 figma properties
+  /// fontFamily: Urbanist
+  /// fontSize: 11px
+  /// height: 150%
+  /// fontWeight: 400
+  /// letterSpacing: 1%
+  /// fontStyle: none
+  /// decoration: none
+  static const TextStyle bodytext10 = TextStyle(
+    fontFamily: 'Urbanist',
+    fontSize: 11,
+    height: 1.5,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.11,
+  );
+}
