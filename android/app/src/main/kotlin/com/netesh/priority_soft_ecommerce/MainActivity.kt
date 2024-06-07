6c82a0e9b53dacbb2ba34047bec46ac59c7fb6a9
Netesh5 <netesh.paudel@gmail.com>
2024-06-07 20:28:16 +0545

    project setup

---
+++ android/app/src/main/kotlin/com/netesh/priority_soft_ecommerce/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.netesh.priority_soft_ecommerce
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/features/homepage/presentation/pages/homepage_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:priority_soft_ecommerce/features/homepage/presentation/widgets/homepage_body.dart';
+
+class HomePageView extends StatelessWidget {
+  const HomePageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const HomePageBody();
+  }
+}
